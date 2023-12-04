d493bdb79f9a17fc3f16c8851592e300268efd61
tokukiyu <tokkiyuam@gmail.com>
2023-12-04 22:34:29 +0300

    done rating

--- backEnd/app/models/bookModel.js
+++ backEnd/app/models/bookModel.js
@@ -89,6 +89,7 @@ bookSchema.methods.addReview = async function (user_id, rating, comment) {
 bookSchema.methods.addRating = async function (rating) {
   if (rating < 1 || rating > 5) {
     throw new Error("Rating must be between 1 and 5.");
+
   }
 
   if (!this.rating || !this.rating[0]) {
