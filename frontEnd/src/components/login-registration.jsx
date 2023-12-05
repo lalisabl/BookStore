2ae8308dbeb0f058a20e967cfb3af2a0b0bacb39
tokukiyu <tokkiyuam@gmail.com>
2023-12-06 01:30:01 +0300

    udpate css of registaration-login page

--- frontEnd/src/components/login-registration.jsx
+++ frontEnd/src/components/login-registration.jsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faEnvelope,
+  faUserAlt,
+  faLock,
+  faCheckCircle,
+  faRocket,
+} from "@fortawesome/free-solid-svg-icons";
 export function Login() {
   return <div></div>;
 }
 
+function placeholder() {
+  return (
+    <>
+      <div>hello h </div>
+    </>
+  );
+}
 export function Register() {
   const [formData, setFormData] = useState({
     fullName: "",
     email: "",
     username: "",
     password: "",
+    confirmPassword: "", // Make sure to include confirmPassword in formData
   });
 
   const handleChange = (e) => {
@@ -46,71 +62,100 @@ export function Register() {
       email: "",
       username: "",
       password: "",
+      confirmPassword: "",
     });
   };
 
   return (
     <div className="registration-container">
-      <Login />
-      <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <label>
-            Full Name:
-            <input
-              type="text"
-              name="fullName"
-              value={formData.fullName}
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <br />
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <br />
-          <label>
-            Username:
-            <input
-              type="text"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <br />
-          <label>
-            Confirm Password:
-            <input
-              type="password"
-              name="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <br />
-          <button type="submit">Register</button>
-        </form>
+      <div className="welcome-banner">
+        <FontAwesomeIcon icon={faRocket} className="icon" />
+        <h3>Welcome </h3>
+        <p>start reading, reader is leader this tooks you 3-5 sec atmost</p>
+        <button className="btn btn-primary-white" type="submit">
+          Login
+        </button>
+      </div>
+      <div className="form-bg">
+        <div className="form-container"></div>
+        <div className="form-content">
+          <h3>Welcome to Gr8Books</h3>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <div>
+                <div className="input-wrapper">
+                  <FontAwesomeIcon icon={faUser} className="icon" />
+                  <input
+                    type="text"
+                    name="fullName"
+                    value={formData.fullName}
+                    onChange={handleChange}
+                    placeholder="Full Name *"
+                    required
+                  />
+                </div>
+                <br />
+                <div className="input-wrapper">
+                  <FontAwesomeIcon icon={faEnvelope} className="icon" />
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="Email *"
+                    required
+                  />
+                </div>
+                <br />
+                <div className="input-wrapper">
+                  <FontAwesomeIcon icon={faUserAlt} className="icon" />
+                  <input
+                    type="text"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                    placeholder="Username *"
+                    required
+                  />
+                </div>
+                <br />
+              </div>
+              <div>
+                <div className="input-wrapper">
+                  <FontAwesomeIcon icon={faLock} className="icon" />
+                  <input
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    placeholder="Password *"
+                    required
+                  />
+                </div>
+                <br />
+                <div className="input-wrapper">
+                  <FontAwesomeIcon icon={faCheckCircle} className="icon" />
+                  <input
+                    type="password"
+                    name="confirmPassword"
+                    value={formData.confirmPassword}
+                    onChange={handleChange}
+                    placeholder="Confirm Password *"
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <h3>google.com</h3>
+            </div>
+            <div>
+              <button className="btn btn-primary" type="submit">
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
