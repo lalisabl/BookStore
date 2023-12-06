0ae6a16eae7db243bef47f418a07477182cfceaa
lbula <bulalalisa@gmail.com>
2023-12-06 17:23:53 -0500

    port number changed

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const app = require("./app/app.js");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT} port`);
 });
