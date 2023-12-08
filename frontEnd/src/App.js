52bbd85c206494475ea59048a0827d753e9eaf81
tokukiyu <tokkiyuam@gmail.com>
2023-12-08 19:37:24 +0300

    update

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -13,7 +13,7 @@ function Pages() {
       </BrowserRouter>
     </>
   );
-}
+} 
 
 function TryMod() {
   const [open, setOpen] = useState(true);
@@ -23,7 +23,7 @@ function TryMod() {
       <GenericModal
         isOpen={open}
         onClose={() => setOpen(false)}
-        children={<Register />}
+        children={<Register />} 
       />
     </>
   );
