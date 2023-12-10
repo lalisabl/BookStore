93af5d7711b936f7677a66a1af8488725a310d65
lbula <bulalalisa@gmail.com>
2023-12-10 14:40:32 -0500

    fullname edit popup added

--- frontEnd/src/components/user/profileDetail.js
+++ frontEnd/src/components/user/profileDetail.js
@@ -1,5 +1,38 @@
+import { useState } from "react";
 import { IoPencil } from "react-icons/io5";
-const profileDetail = () => {
+import { MdOutlineClear } from "react-icons/md";
+const ProfileDetail = () => {
+  const [showFullNamePopup, setShowFullNamePopup] = useState(false);
+  const [showPasswordPopup, setShowPasswordPopup] = useState(false);
+  const [fullName, setFullName] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState("");
+
+  const handleEditFullName = () => {
+    setShowFullNamePopup(true);
+  };
+
+  const handleEditPassword = () => {
+    setShowPasswordPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowFullNamePopup(false);
+    setShowPasswordPopup(false);
+  };
+
+  const handleSaveFullName = () => {
+    // Add logic to save the full name
+    console.log("Saving Full Name:", fullName);
+    handleClosePopup();
+  };
+
+  const handleSavePassword = () => {
+    // Add logic to save the new password
+    console.log("Saving Password:", newPassword);
+    handleClosePopup();
+  };
   return (
     <div>
       <div className="main-container">
@@ -7,7 +40,9 @@ const profileDetail = () => {
         <div className="user-details">
           <div className="detail-item fname">
             <p>Full name</p>
-            <button className="btn">Edit</button>
+            <button onClick={handleEditFullName} className="btn">
+              Edit
+            </button>
           </div>
           <div className="detail-item username">
             <p>User name</p>
@@ -15,7 +50,9 @@ const profileDetail = () => {
           </div>
           <div className="detail-item password">
             <p>Password</p>
-            <button className="btn">Edit </button>
+            <button onClick={handleEditPassword} className="btn">
+              Edit{" "}
+            </button>
           </div>
           <div className="detail-item e-mail">
             <p>Email</p>
@@ -23,8 +60,97 @@ const profileDetail = () => {
           </div>
         </div>
       </div>
+      {showFullNamePopup && (
+        <FullNameUpdate
+          fullName={fullName}
+          setFullName={setFullName}
+          onSave={handleSaveFullName}
+          onClose={handleClosePopup}
+        />
+      )}
+      {showPasswordPopup && (
+        <PasswordUpdate
+          currentPassword={currentPassword}
+          setCurrentPassword={setCurrentPassword}
+          newPassword={newPassword}
+          setNewPassword={setNewPassword}
+          confirmNewPassword={confirmNewPassword}
+          setConfirmNewPassword={setConfirmNewPassword}
+          onSave={handleSavePassword}
+          onClose={handleClosePopup}
+        />
+      )}
+    </div>
+  );
+};
+const FullNameUpdate = ({ fullName, setFullName, onSave, onClose }) => {
+  return (
+    <div className="popup fname">
+      <h3>Profile Full Name</h3>
+      <button className="btn" onClick={onClose}>
+        <MdOutlineClear />
+      </button>
+      <input
+        type="text"
+        value={fullName}
+        onChange={(e) => setFullName(e.target.value)}
+        placeholder="your full name"
+      />
+      <button className="btn" onClick={onSave}>
+        Save
+      </button>
     </div>
   );
 };
 
-export default profileDetail;
+const PasswordUpdate = ({
+  currentPassword,
+  setCurrentPassword,
+  newPassword,
+  setNewPassword,
+  confirmNewPassword,
+  setConfirmNewPassword,
+  onSave,
+  onClose,
+}) => {
+  return (
+    <div className="popup pswd">
+      <h3>Edit Password</h3>
+      <button className="btn" onClick={onClose}>
+        <MdOutlineClear />
+      </button>
+      <label>
+        Current Password:
+        <input
+          type="password"
+          value={currentPassword}
+          onChange={(e) => setCurrentPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        New Password:
+        <input
+          type="password"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        Confirm New Password:
+        <input
+          type="password"
+          value={confirmNewPassword}
+          onChange={(e) => setConfirmNewPassword(e.target.value)}
+        />
+      </label>
+      <button className="btn" onClick={onSave}>
+        Save
+      </button>
+      <button className="btn" onClick={onClose}>
+        Cancel
+      </button>
+    </div>
+  );
+};
+
+export default ProfileDetail;
