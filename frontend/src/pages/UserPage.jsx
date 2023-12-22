7ee0d50e15bd9ade3ec8063953c3183ef75e29f1
tokukiyu <tokkiyuam@gmail.com>
2023-12-22 11:35:36 +0300

    u

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -9,6 +9,6 @@ export function UserPage({ SetLogin, path }) {
       <div className="mt-14 items-center flex-grow p-0 lg:ml-16 md:ml-16 pl-0 sm:pl-2 sm:ml-10 lg:w-5/6 md:w-5/6 ml-0 sm:w-5/6 w-screen overflow-hidden ">
         {path}
       </div>
-    </div> 
+    </div>  
   );
 }
