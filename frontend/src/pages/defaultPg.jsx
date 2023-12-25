676a8542cf34d09c3fadae82a4529858f0323b73
tokukiyu <tokkiyuam@gmail.com>
2023-12-25 21:08:50 +0300

    udpate

--- frontend/src/pages/defaultPg.jsx
+++ frontend/src/pages/defaultPg.jsx
@@ -1,12 +1,22 @@
+import { FaArrowLeft } from "react-icons/fa";
 import UserNav from "../components/user/userNav";
 
 export default function DefaultPage({ page }) {
+  const handleGoBack = () => {
+    window.history.back();
+  };
   return (
-    <div >
+    <div>
       <UserNav />
-      <div className="mt-14  landing-pg">
-        {page}
-      </div>
+      <button
+        className="fixed z-30 border left-3 flex items-center"
+        onClick={handleGoBack}
+      >
+        <FaArrowLeft className="text-xl" />
+        Back
+      </button>
+
+      <div className="mt-14  landing-pg">{page}</div>
     </div>
   );
 }
